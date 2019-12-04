ac00516c10418b86b24ae83a2d8df15a0fdcd194
Thys B <dicebros1982@gmail.com>
2019-12-04 18:21:38 +0100

    created login form

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  inputForm: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.inputForm = new FormGroup({
+      email: new FormControl('', { validators: [Validators.required, Validators.email] }),
+      password: new FormControl('', Validators.required)
+    });
   }
 
+  onSubmit() {
+    console.log(this.inputForm.value);
+  }
 }
--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from "@angular/core";
-import { MaterialModule } from "../material/material.module";
-import { FlexModule } from "../flex/flex.module";
+import { NgModule } from '@angular/core';
+import { MaterialModule } from '../material/material.module';
+import { FlexModule } from '../flex/flex.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
-const coreModules = [MaterialModule, FlexModule];
+const coreModules = [MaterialModule, FlexModule, ReactiveFormsModule];
 
 @NgModule({
   imports: [coreModules],
